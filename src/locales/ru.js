e881b3e3cd744cd38a67b7bccac9a0a0368c260c
NickyEnglish2 <ilya.fomin.99.62@gmail.com>
2025-03-04 15:28:25 +0800

    Moved locales into their own directory

---
+++ src/locales/ru.js
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    feedback: {
+      failed: 'Произошла ошибка...',
+      loading: 'Загрузка...',
+      invalid: 'Ссылка должна быть валидным URL',
+      success: 'RSS успешно загружен',
+      alreadyExists: 'RSS уже существует',
+      networkErr: 'Ошибка сети',
+      notContaining: 'Ресурс не содержит валидный RSS',
+      unknownErr: 'Неизвестная ошибка',
+      required: 'Не должно быть пустым',
+    },
+    watchBtn: 'Просмотр.',
+  },
+};

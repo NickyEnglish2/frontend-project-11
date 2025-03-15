355a90be4004537d4c834dbb52fbf31e916bb97d
NickyEnglish2 <ilya.fomin.99.62@gmail.com>
2025-03-15 11:54:33 +0800

    Removed

--- src/view.js
+++ src/view.js
@@ -18,7 +18,6 @@ const renderStatus = (state, elements, i18nInstance) => {
       feedback.classList.remove('text-success');
       feedback.classList.add('text-danger');
       input.classList.add('is-invalid');
-      input.value = '';
       break;
     case 'success':
       submitBtn.removeAttribute('disabled');
